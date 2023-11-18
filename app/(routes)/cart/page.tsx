d96b69d84abb271e28b346de7b57542fc4e08986
Théo <theo.fabianomattei@gmail.com>
2023-11-18 01:31:56 +0100

    feat: add cart components and page

---
+++ app/(routes)/cart/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { ReactElement, useEffect, useState } from "react";
+
+import Container from "@/components/ui/container";
+import useCart, { CartStore } from "@/hooks/use-cart";
+
+import Summary from "./components/summary";
+import CartItem from "./components/cart-item";
+import { Product } from "@/types";
+
+export const revalidate = 0;
+
+function CartPage(): ReactElement | null {
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+  const cart: CartStore = useCart();
+
+  useEffect((): void => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-16 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
+          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+            <div className="lg:col-span-7">
+              {cart.items.length === 0 && (
+                <p className="text-neutral-500">No items added to cart.</p>
+              )}
+              <ul>
+                {cart.items.map(
+                  (item: Product): ReactElement => (
+                    <CartItem
+                      key={item.id}
+                      data={item}
+                    />
+                  )
+                )}
+              </ul>
+            </div>
+            <Summary />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default CartPage;
